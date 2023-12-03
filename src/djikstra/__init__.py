a734e29a8392556c44c8d91ffc6798ab7c7760e2
Tom <im.tomtomon@gmail.com>
2023-12-03 22:56:00 +0000

    Update __init__.py

--- src/djikstra/__init__.py
+++ src/djikstra/__init__.py
@@ -7,31 +7,3 @@ See https://github.com/TOMG-A/djikstra/README.md for documentation
 
 from djikstra.djikstra import *
 from djikstra.node import *
-
-
-## Test Graph
-# A = Node("A")
-# B = Node("B")
-# C = Node("C")
-# D = Node("D")
-# E = Node("E")
-# F = Node("F")
-# G = Node("G")
-# H = Node("H")
-# I = Node("I")
-# J = Node("J")
-# A.append(B,3)
-# A.append(D,5)
-# B.append(E,7)
-# C.append(F,4)
-# D.append(H,2)
-# D.append(E,6)
-# E.append(C,2)
-# E.append(F,3)
-# F.append(G,1)
-# H.append(E,4)
-# H.append(G,5)
-# H.append(I,6)
-# I.append(J,1)
-# G.append(J,4)
-# GRAPH=Graph([A,B,C,D,E,F,G,H,I,J])
